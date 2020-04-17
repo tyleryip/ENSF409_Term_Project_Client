1e42addee4bf72c2724111473865f9dfc20af10e
Tyler Yip <tyler.yip1@ucalgary.ca>
2020-04-17 16:41:52 -0600

    generated docs

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.KerrYip.ClientApplication"},{"l":"com.KerrYip.ClientController"},{"l":"com.KerrYip.ClientView"},{"l":"com.KerrYip.Model"}]
