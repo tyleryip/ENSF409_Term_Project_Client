b39d1fe1c31f940f921bdc5f582ed8c9a607ced4
Tyler Yip <tyler.yip1@ucalgary.ca>
2020-04-18 14:17:55 -0600

    fixed java docs

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.KerrYip.ClientApplication"},{"l":"com.KerrYip.ClientController"},{"l":"com.KerrYip.ClientView"},{"l":"com.KerrYip.Model"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.KerrYip.ClientApplication"},{"l":"com.KerrYip.ClientController"},{"l":"com.KerrYip.ClientView"},{"l":"com.KerrYip.Model"},{"l":"com.KerrYip.ServerApplication"},{"l":"com.KerrYip.ServerController"},{"l":"test.KerrYip"}]
